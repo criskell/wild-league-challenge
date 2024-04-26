3f9b60e42307fcd4f2cce98a7821e944236dd832
criskell <cris.moraes.ds@gmail.com>
2024-04-25 23:41:37 -0300

    feat: add help

--- src/main.lua
+++ src/main.lua
@@ -2,4 +2,12 @@ local commands = require "src.commands"
 
 command = arg[1]
 
-commands[command]()
+handler = commands[command]
+
+if not handler then
+    print("Uso:")
+    print(" add <descrição da tarefa>: Adiciona uma tarefa")
+    print(" remove <id da tarefa>: Remove uma tarefa")
+    print(" toggle <id da tarefa>: Alterna a tarefa entre o estado pendente e concluído.")
+    print(" list: Mostra todas as tarefas")
+end
